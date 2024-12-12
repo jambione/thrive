f889fa2ba48b0a58e55494423e04f2079c861386
Jambione <jon@jbrasfield.com>
2024-12-12 05:50:36 -0500

    adding live reload and nodemon

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname)));
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname);
+
+app.use(connectLivereload());
 
 // Route for serving index.html
 app.get("/", (req, res) => {
